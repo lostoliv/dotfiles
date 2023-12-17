2745d9ef34b1a7325ca1c3aa4c34160d5f155f9f
oliv <oliv@oliv>
2023-12-17 00:10:15 -0800

    dotfiles

---
+++ bin/sysclean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+if [ "$(uname)" == "Darwin" ]; then
+  find ~/ -type f \( -name ".DS_Store" -o -name ".localized" -o -name "._*" \) -exec rm -f {} \;
+  rm -rf ~/Library/Logs/*
+  qlmanage -r cache
+fi
